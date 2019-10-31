5fa1c1afe721881b0c8ab6b8070df18f9f2abc14
Stacy <enjoyorhate@gmail.com>
2019-10-31 23:41:00 +0200

    added Form component layout

--- src/components/Admin_table.js
+++ src/components/Admin_table.js
@@ -2,19 +2,24 @@ export default class AdminTable {
   constructor() {
     this.product = document.querySelector(".product");
     this.API = "https://5db95beaeddc81001495eb0f.mockapi.io/api/products";
+    this.createBtn = document.getElementById("create");
+    // this.createBtn.addEventListener("click", form.handleCreate);
   }
+
   getProducts = () => {
     fetch(this.API)
-      .then(res => res.json())
+      .then(res => {
+        if (res.ok) return res.json();
+        throw Error(response.status + " " + response.statusText);
+      })
       .then(data => this.showProducts(data))
       .catch(error => console.log(`ERROR ${error}`));
   };
   showProducts = list => {
-    console.log(list);
     const productList = list.reduce(
       (acc, item) =>
         acc +
-        `<tr><td>${item.id}</td><td>${item.name}</td><td>${item.desc}</td><td>${item.price}</td><td>${item.available}</td><td>${item.img}</td><tr>`,
+        `<tr><td>${item.id}</td><td>${item.name}</td><td>${item.desc}</td><td>${item.price}</td><td>${item.available}</td><td><img src=${item.img} alr=${item.name} width="50"></td><tr>`,
       ""
     );
     this.product.innerHTML = productList;
